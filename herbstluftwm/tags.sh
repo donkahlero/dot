323347cb67260354556ded619491fd2b17757ced
TacoVox <jonas.kahler@icloud.com>
2016-02-14 00:43:58 +0100

    Font size fix and removal of white spaces

--- herbstluftwm/tags.sh
+++ herbstluftwm/tags.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 tw=80 expandtab :
-# Florian Bruhin <me@the-compiler.org>
-
 dzen_fg="#D0D0D0"
 dzen_bg="#1C1C1C"
 normal_fg=""
@@ -16,24 +13,24 @@ used_bg=
 herbstclient --idle 2>/dev/null | {
     tags=( $(herbstclient tag_status) )
     while true; do
-	for tag in "${tags[@]}" ; do
-	    case ${tag:0:1} in
-		'#') cstart="^fg($viewed_fg)^bg($viewed_bg)" ;;
-		'+') cstart="^fg($viewed_fg)^bg($viewed_bg)" ;;
-		':') cstart="^fg($used_fg)^bg($used_bg)"     ;;
-		'!') cstart="^fg($urgent_fg)^bg($urgent_bg)" ;;
-		*)   cstart=''                               ;;
-	    esac
-	    dzenstring="${cstart}^ca(1,herbstclient use ${tag:1}) ${tag:1} "
-	    dzenstring+="^ca()^fg()^bg()"
-	    echo -n "$dzenstring"
-	done
-	echo
-	read hook || exit
-	case "$hook" in
-	    tag*) tags=( $(herbstclient tag_status) ) ;;
-	    quit_panel*) exit ;;
-	esac
+        for tag in "${tags[@]}" ; do
+            case ${tag:0:1} in
+                '#') cstart="^fg($viewed_fg)^bg($viewed_bg)" ;;
+                '+') cstart="^fg($viewed_fg)^bg($viewed_bg)" ;;
+                ':') cstart="^fg($used_fg)^bg($used_bg)"     ;;
+                '!') cstart="^fg($urgent_fg)^bg($urgent_bg)" ;;
+                *)   cstart=''                               ;;
+            esac
+            dzenstring="${cstart}^ca(1,herbstclient use ${tag:1}) ${tag:1} "
+            dzenstring+="^ca()^fg()^bg()"
+            echo -n "$dzenstring"
+        done
+        echo
+        read hook || exit
+        case "$hook" in
+            tag*) tags=( $(herbstclient tag_status) ) ;;
+            quit_panel*) exit ;;
+        esac
     done
-} | dzen2 -h 32 -fn '-*-profont-*-*-*-*-11-*-*-*-*-*-*-*' -ta l -sa l \
-	            -w 2560 -fg "$dzen_fg" -bg "$dzen_bg" -e 'button3='
+} | dzen2 -h 32 -fn '-*-profont-*-*-*-*-14-*-*-*-*-*-*-*' -ta l -sa l \
+                    -w 2560 -fg "$dzen_fg" -bg "$dzen_bg" -e 'button3='
