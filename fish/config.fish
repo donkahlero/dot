4da8e843c526e626a255412db050977202e59640
TacoVox <jonas@derkahler.de>
2018-02-27 14:50:11 +0100

    Lets start with :fish:

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+set -Ux COLORTERM truecolor
+set -Ux TERM xterm-kitty
+set -Ux EDITOR vim
+
+thefuck --alias | source
+alias 1live='mplayer https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3'
+alias hr1='mplayer http://hr-hr1-live.cast.addradio.de/hr/hr1/live/mp3/128/stream.mp3'
