0691fe11336912494b5e6ebfa41268d8a567a112
Jonas Kahler <jonas@TacoBook.local>
2018-03-01 21:27:59 +0100

    Added OS specific clauses

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,13 @@ set -Ux COLORTERM truecolor
 set -Ux TERM xterm-kitty
 set -Ux EDITOR vim
 
-thefuck --alias | source
+switch (uname)
+case Linux
+    # Linux stuff
+    thefuck --alias | source
+echo Darwin
+    # macOS specific stuff
+end
+
 alias 1live='mplayer https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3'
 alias hr1='mplayer http://hr-hr1-live.cast.addradio.de/hr/hr1/live/mp3/128/stream.mp3'
