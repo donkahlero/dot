49e2cd5949bda87513cda81cb8a0a401ac8c4d35
TacoVox <jonas@derkahler.de>
2018-10-13 19:09:16 +0200

    Adds lemonbar
    New and improved setup using lemonbar. No need for dzen2 any longer.
    Slimmer.
    
    Completely based on the Panda sytax theme.
    
    Requirements will be mentioned in README.md

--- lemonbar/run.sh
+++ lemonbar/run.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-$(~/.config/lemonbar/bar.sh | lemonbar -g 2560x30+0+0)
+~/.config/lemonbar/bar.sh | \
+    lemonbar -n herbstluftwm \
+    -g 1920x30+0+0\
+    -o -8 -f "MiniMono:size=10"\
+    -o -7 -f "Typicons::size=11" | \
+    while read line; do eval "$line"; done &
 
