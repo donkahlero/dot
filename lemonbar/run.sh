c638b9965f3014f0c4081dd19ce21a64cf1c851f
TacoVox <3264648+TacoVox@users.noreply.github.com>
2023-01-09 08:38:44 +0100

    chore: remove lemonbar
    ...in favor of polybar

--- lemonbar/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-~/.config/lemonbar/bar.sh | \
-    lemonbar -n herbstluftwm \
-    -g 1920x30+0+0\
-    -o -8 -f "MiniMono:size=10"\
-    -o -7 -f "Typicons::size=11" | \
-    while read line; do eval "$line"; done &
-
