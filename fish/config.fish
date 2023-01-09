374c95dc6abaf2c7f00c59d4c2f74d88b0970af9
TacoVox <3264648+TacoVox@users.noreply.github.com>
2023-01-09 08:38:19 +0100

    feat(fish): add startship support

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set -x COLORTERM truecolor
 set -x TERM xterm-kitty
-set -x EDITOR vim
+set -x EDITOR nvim
 
 set -x LC_COLLATE en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
@@ -10,10 +10,12 @@ set -x LC_NUMERIC en_US.UTF-8
 set -x LC_TIME en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 
-set -x PATH $PATH /Users/jonas/go/bin /usr/local/bin /Users/jonas/Library/Python/3.8/bin
-set -x GOPATH /Users/jonas/go
+set -x PATH $PATH /home/jonas/go/bin /usr/local/bin /home/jonas/.local/bin /opt/gcc-arm-none-eabi-10.3-2021.10/bin/
+set -x GOPATH /home/jonas/go
 
+set -gx GPG_TTY (tty)
+gpg-connect-agent updatestartuptty /bye >/dev/null
+
+starship init fish | source
 kitty + complete setup fish | source
 
-alias 1live='mplayer https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3'
-alias hr1='mplayer http://hr-hr1-live.cast.addradio.de/hr/hr1/live/mp3/128/stream.mp3'
