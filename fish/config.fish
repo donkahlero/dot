a5d7309cd6e8d04539a7d0a20bc69cafd08e69a9
TacoVox <3264648+TacoVox@users.noreply.github.com>
2023-03-24 07:29:07 +0100

    chore(fish): remove unused paths, remove starship

--- fish/config.fish
+++ fish/config.fish
@@ -10,13 +10,11 @@ set -x LC_NUMERIC en_US.UTF-8
 set -x LC_TIME en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 
-set -x PATH $PATH /home/jonas/go/bin /usr/local/bin /home/jonas/.local/bin /opt/gcc-arm-none-eabi-10.3-2021.10/bin/
+set -x PATH $PATH /home/jonas/go/bin /usr/local/bin /home/jonas/.local/bin
 set -x GOPATH /home/jonas/go
 
 set -gx GPG_TTY (tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
-
-starship init fish | source
 
 pyenv init - | source
 status --is-interactive; and pyenv virtualenv-init - | source
