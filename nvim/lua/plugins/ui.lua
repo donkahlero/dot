52fc0778b494e70833ca6489e07db12fdd82dfe7
donkahlero <3264648+donkahlero@users.noreply.github.com>
2025-12-19 06:11:07 +0100

    feat(nvim): add cursor smur

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -374,4 +374,15 @@ return {
       require("scrollbar").setup()
     end,
   },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      cursor_color = "#d3cdc3",
+      normal_bg = "#282828",
+      smear_between_buffers = true,
+      smear_between_neighbor_lines = true,
+      use_floating_windows = true,
+      legacy_computing_symbols_support = false,
+    },
+  },
 }
